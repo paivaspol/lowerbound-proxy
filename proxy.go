f11fc7dea67268240e4a58897095b11d58a55cbf
Vaspol Ruamviboonsuk <vaspol@umich.edu>
2017-12-31 10:43:05 -0500

    Switch to use github.com/elazarl/goproxy instead of vanilla go http library.

--- proxy.go
+++ proxy.go
@@ -1,31 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+
+	"github.com/elazarl/goproxy"
 )
+
+func orPanic(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	port := flag.Int("port", 8443, "The port this proxy should listen to")
-	certFile := flag.String("cert_file", "./cert.pem", "The certificate this proxy should use")
-	keyFile := flag.String("key_file", "./key.pem", "The key this proxy should use")
 	flag.Parse()
 
 	log.Printf(fmt.Sprintf("Starting proxy on %d\n", *port))
 
-	http.HandleFunc("/", handleHTTP)
-	log.Fatal(http.ListenAndServeTLS(fmt.Sprintf(":%d", *port), *certFile, *keyFile, nil))
-}
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = true
+	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
+	proxy.OnRequest().HijackConnect(func(req *http.Request, client net.Conn, ctx *goproxy.ProxyCtx) {
+		log.Printf("req: %v", req)
+		defer func() {
+			if e := recover(); e != nil {
+				ctx.Logf("error connecting to remote: %v", e)
+				client.Write([]byte("HTTP/1.1 500 Cannot reach destination\r\n\r\n"))
+			}
+			client.Close()
+		}()
+		clientBuf := bufio.NewReadWriter(bufio.NewReader(client), bufio.NewWriter(client))
+		remote, err := net.Dial("tcp", req.URL.Host)
+		orPanic(err)
+		remoteBuf := bufio.NewReadWriter(bufio.NewReader(remote), bufio.NewWriter(remote))
+		for {
+			req, err := http.ReadRequest(clientBuf.Reader)
+			orPanic(err)
+			orPanic(req.Write(remoteBuf))
+			orPanic(remoteBuf.Flush())
+			resp, err := http.ReadResponse(remoteBuf.Reader, req)
+			orPanic(err)
+			orPanic(resp.Write(clientBuf.Writer))
+			orPanic(clientBuf.Flush())
+		}
+	})
 
-// handleHTTP handles all requests that goes through this proxy.
-//
-// In this setup, the proxy can respond to a request in two ways:
-// 	(1) Serve the request from a recorded file
-// 	(2) Serve passthrough the request to the wide area network.
-func handleHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf("[PROXY] Handling request: %v\n", r.URL.String())
-	w.WriteHeader(200)
-	w.Write([]byte("hello!\n"))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), proxy))
 }
