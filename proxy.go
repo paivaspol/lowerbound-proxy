16ca606a5364838414bb7c2820cd3186b9edd301
Vaspol Ruamviboonsuk <vaspol@umich.edu>
2017-12-31 11:47:14 -0500

    MITM all requests

--- proxy.go
+++ proxy.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"log"
-	"net"
 	"net/http"
 
 	"github.com/elazarl/goproxy"
@@ -19,37 +17,17 @@ func orPanic(err error) {
 
 func main() {
 	port := flag.Int("port", 8443, "The port this proxy should listen to")
+	verbose := flag.Bool("verbose", false, "Whether to verbosely log the proxy")
 	flag.Parse()
 
 	log.Printf(fmt.Sprintf("Starting proxy on %d\n", *port))
 
 	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
+	// proxy.Verbose = true
 	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
-	proxy.OnRequest().HijackConnect(func(req *http.Request, client net.Conn, ctx *goproxy.ProxyCtx) {
-		log.Printf("req: %v", req)
-		defer func() {
-			if e := recover(); e != nil {
-				ctx.Logf("error connecting to remote: %v", e)
-				client.Write([]byte("HTTP/1.1 500 Cannot reach destination\r\n\r\n"))
-			}
-			client.Close()
-		}()
-		clientBuf := bufio.NewReadWriter(bufio.NewReader(client), bufio.NewWriter(client))
-		remote, err := net.Dial("tcp", req.URL.Host)
-		orPanic(err)
-		remoteBuf := bufio.NewReadWriter(bufio.NewReader(remote), bufio.NewWriter(remote))
-		for {
-			req, err := http.ReadRequest(clientBuf.Reader)
-			orPanic(err)
-			orPanic(req.Write(remoteBuf))
-			orPanic(remoteBuf.Flush())
-			resp, err := http.ReadResponse(remoteBuf.Reader, req)
-			orPanic(err)
-			orPanic(resp.Write(clientBuf.Writer))
-			orPanic(clientBuf.Flush())
-		}
+	proxy.OnRequest().DoFunc(func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+		log.Printf("req: %v", r)
+		return r, goproxy.NewResponse(r, goproxy.ContentTypeText, http.StatusOK, "Oops")
 	})
-
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), proxy))
 }
