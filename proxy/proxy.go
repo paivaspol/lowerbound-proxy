9e609700cc49efb68c7217ea869e2c3da5ad7d6d
Vaspol Ruamviboonsuk <vaspol@umich.edu>
2017-12-31 21:02:05 -0500

    Add logic for reading from file for important ressources.

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -10,14 +10,9 @@ import (
 	"github.com/paivaspol/lowerboundproxy"
 )
 
-func orPanic(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
 func main() {
 	port := flag.Int("port", 8443, "The port this proxy should listen to")
+	importantURLFile := flag.String("important-urls", "./important", "The filye containing the important URLs delimited by newline")
 	verbose := flag.Bool("verbose", false, "Whether to verbosely log the proxy")
 	flag.Parse()
 
@@ -27,6 +22,11 @@ func main() {
 	defer func() {
 		resourceQueue.Cleanup()
 	}()
+
+	importantURLs, err := getImportantURLs(*importantURLFile)
+	if err != nil {
+		log.Fatalf("failed to get important URLs: %v", err)
+	}
 
 	proxyHandler := goproxy.NewProxyHttpServer()
 	proxyHandler.Verbose = *verbose
@@ -38,10 +38,34 @@ func main() {
 	proxyHandler.OnResponse().DoFunc(func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
 		log.Printf("[In Response] req: %v", r.Request.URL)
 		signalChan := make(chan bool)
-		resourceQueue.QueueRequest(lowerboundproxy.High, r.Request.URL.String(), signalChan)
+		priority := lowerboundproxy.Low
+		if _, ok := importantURLs[r.Request.URL.String()]; ok {
+			priority = lowerboundproxy.High
+		}
+		resourceQueue.QueueRequest(priority, r.Request.URL.String(), signalChan)
+
 		// Block until we get a go from the queue.
 		<-signalChan
 		return r
 	})
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), proxyHandler))
+}
+
+// getImportantURLs reads the URLs from the given file. The file is assumed to
+// contain URLs separated by new line characters.
+func getImportantURLs(importantURLFile string) (map[string]bool, err) {
+	file, err := os.Open(importantURLFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	importantURLs = make(map[string]bool)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		importantURL[scanner.Text()] = true
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return importantURLs, nil
 }
